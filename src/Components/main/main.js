9bb7708470f7ab10991da63c6f35254218531cd4
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-06-08 19:36:48 +0300

    start

---
+++ src/Components/main/main.js
@@ -0,0 +1,10 @@
+const MainPage = () => {
+    return (
+        <div>
+            <h2>hello</h2>
+            <h2>hello</h2>
+            <h2>hello</h2>
+        </div>
+    )
+}
+export default MainPage;
